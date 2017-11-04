3cb980d02b70f1090954b2c2f38894d053091874
Aung Pyae Phyo <aungpyaephyo24@gmail.com>
2017-11-04 17:48:50 +0630

    Initialize skeleton projects with default structure / value.

---
+++ app/src/main/java/com/padcmyanmar/sfc/SFCNewsApp.java
@@ -0,0 +1,17 @@
+package com.padcmyanmar.sfc;
+
+import android.app.Application;
+
+/**
+ * Created by aung on 11/4/17.
+ */
+
+public class SFCNewsApp extends Application {
+
+    public static final String LOG_TAG = "SFCNewsApp";
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
